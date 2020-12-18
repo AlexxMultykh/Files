68ca013cc94a5b57961bb078029c138e26daa6cc
unknown <smultyh@gmail.com>
2020-12-18 20:53:26 +0200

    Files

---
+++ Files/Files/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the name of the file");
+            string file_name = Console.ReadLine();
+            Console.WriteLine(File.ReadAllText(file_name));
+        }
+    }
+}
